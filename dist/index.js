c97213a9cc6378ea4ebe9b924499c44b9ac0b5b4
Axel Niklasson <axel@mobile.dev>
2022-11-17 14:37:36 +0100

    Version 1.1.1

--- dist/index.js
+++ dist/index.js
@@ -45594,11 +45594,11 @@ function getParameters() {
         var env = {};
         env = core.getMultilineInput('env', { required: false })
             .map(it => {
-            const pair = it.split("=");
-            if (pair.length != 2) {
+            const parts = it.split('=');
+            if (parts.length < 2) {
                 throw new Error(`Invalid env parameter: ${it}`);
             }
-            return { key: pair[0], value: pair[1] };
+            return { key: parts[0], value: parts.slice(1).join('=') };
         })
             .reduce((map, entry) => {
             map[entry.key] = entry.value;

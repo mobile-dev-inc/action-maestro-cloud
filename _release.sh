276e91298b059b5508fd38b7cf82c0ebffe41eb7
Axel Niklasson <axel@mobile.dev>
2023-05-23 10:20:01 +0200

    Update release script to also update major version tag

--- _release.sh
+++ _release.sh
@@ -3,6 +3,7 @@
 set -e
 
 VERSION=$1
+MAJOR_VERSION=$(echo "$version" | cut -d '.' -f 1)
 
 if [[ -z "$VERSION" ]]; then
   echo "Usage: npm run release <version>"
@@ -16,3 +17,6 @@ git commit --allow-empty -m "Version ${VERSION}"
 git tag -a "v${VERSION}" -m "Version ${VERSION}"
 git push
 git push --tags
+# update major version tag
+git tag -fa "v${MAJOR_VERSION}" -m "Update v${MAJOR_VERSION} tag"
+git push origin "v${MAJOR_VERSION}" --force
